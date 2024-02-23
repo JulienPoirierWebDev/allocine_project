d0a2f5323cfe347654c612b3cadebf0c2b4bb543
androd22 <andy_14@hotmail.fr>
2024-02-23 17:22:41 +0100

    creation getMovies, postMovies, putMovies

--- projetApiCrud/index.js
+++ projetApiCrud/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.json())
+
 let movies = [
     {
         id : 1,
@@ -14,15 +16,44 @@ let movies = [
     {
         id : 3,
         title : "le seigneur des anneaux : le retour du roi"
-    }
+    },
+  
 ]
-
+ 
 app.get("/projetcrud", (request, response)=>{
     response.send("hello le monde")
 })
 app.get("/api/movies", (request, response)=>{
     response.json({ 
         data : movies
+    })
+})
+
+app.post("/api/movies",(request, response)=> {
+    const newMovie = {
+        id : Date.now(),
+        title : request.body.title
+    }
+    movies.push(newMovie)
+    response.json({
+        message : "le film a bien été ajouté", newMovie, error : false
+    })
+})
+
+app.put("/api/movies/:id", (request, response)=>{
+    movies = movies.map((movie)=>{
+        if(movie.id === Number(request.params.id)){
+            return {
+                id : Number(request.params.id),
+                title : request.body.title
+            }
+        }else{
+            return movie;
+        }
+    })
+    response.json({
+        message : "le titre a bien été modifié",
+        error : false,   
     })
 })
 app.listen(3000, (error)=>{
