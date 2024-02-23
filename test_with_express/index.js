5f0e12ca819d2b833a91bea335bc16afae635979
JulienPoirierWebDev <connexion@julienpoirier-Webdev.com>
2024-02-23 15:22:49 +0100

    crud without db

--- test_with_express/index.js
+++ test_with_express/index.js
@@ -2,28 +2,101 @@ const express = require("express");
 
 const app = express();
 
+let data = [
+  {
+    id: 1,
+    name: "Julien",
+    email: "test@test.com",
+    password: "1234",
+    isAdmin: false,
+  },
+  {
+    id: 2,
+    name: "Bob",
+    email: "bob@test.com",
+    password: "1234",
+    isAdmin: true,
+  },
+];
+
+// middleware pour accepter le json dans le body d'une requête.
+app.use(express.json());
+
+app.use((request, response, next) => {
+  next();
+});
+
 app.get("/", (request, response) => {
   response.send("Vous êtes sur la page principale");
 });
 
-app.post("/", (request, response) => {
-  response.json({ message: "Ceci est une requête post" });
+app.get("/api/users", (request, response) => {
+  response.status(200).json({ data: data });
 });
 
-app.get("/hello", (request, response) => {
-  response.send("Hello");
+app.get("/api/users/:id", (req, res) => {
+  if (isNaN(Number(req.params.id))) {
+    res.json({
+      message: "l'id n'est pas un nombre !!",
+      error: true,
+    });
+  }
+
+  const searchUser = data.find((user) => user.id === Number(req.params.id));
+
+  res.json({ data: searchUser, error: false });
 });
 
-app.get("/goodbye", (request, response) => {
-  response.send("goodbye");
+app.post("/api/users", (req, res) => {
+  const newUser = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    id: Date.now(),
+  };
+
+  data.push(newUser);
+
+  res.json({ message: "Utilisateur enregistré dans la bdd", error: false });
 });
 
-app.get("/aha", (request, response) => {
-  response.send("aha");
+app.put("/api/users", (req, res) => {
+  const updatedUser = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    id: Number(req.body.id),
+  };
+
+  const newData = data.map((user) => {
+    console.log(user.id, updatedUser.id);
+    if (user.id === updatedUser.id) {
+      console.log(updatedUser);
+      return updatedUser;
+    } else {
+      return user;
+    }
+  });
+
+  data = newData;
+
+  res.json({ message: "Utilisateur mis a jour", error: false });
 });
 
-app.get("/doc", (request, response) => {
-  response.send("doc");
+app.delete("/api/users/:id", (req, res) => {
+  // Si l'utilisateur existe, on le supprime. Sinon, on informe que l'utilisateur ciblé n'existe pas.
+
+  const userExist = data.find((user) => user.id === Number(req.params.id));
+
+  if (!userExist) {
+    res
+      .status(204)
+      .json({ message: "L'utilisateur n'existe pas", error: true });
+  }
+
+  data = data.filter((user) => user.id !== Number(req.params.id));
+
+  res.json({ message: "utilisateur supprimé", error: false });
 });
 
 app.listen("3001", (err) => {
