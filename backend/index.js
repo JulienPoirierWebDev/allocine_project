d59b9ba2a028ebb00f05972fb8a34bedbe9d9338
JulienPoirierWebDev <connexion@julienpoirier-Webdev.com>
2024-02-25 09:21:23 +0100

    add and update movie

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,8 @@ const userSchema = new mongoose.Schema({
   photo: String,
   biography: String,
   role: String,
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  moviesList: { type: mongoose.Schema.Types.ObjectId, ref: "MovieList" },
 });
 
 const User = mongoose.model("User", userSchema);
@@ -20,12 +22,9 @@ const User = mongoose.model("User", userSchema);
 const movieSchema = new mongoose.Schema({
   TMDBId: Number,
   title: String,
-  year: Number,
+  date: String,
   poster: String,
   description: String,
-  category: String,
-  director: String,
-  duration: Number,
   rating: Number,
 });
 
@@ -36,7 +35,8 @@ const movieListSchema = new mongoose.Schema({
   movies: [
     {
       id: { type: mongoose.Schema.Types.ObjectId, ref: "Movie" },
-      status: { type: String, enum: ["watched", "to-watch", "favorite"] },
+      status: { type: String, enum: ["vu", "a voir"] },
+      TMDBId: Number,
     },
   ],
 });
@@ -46,6 +46,109 @@ const MovieList = mongoose.model("MovieList", movieListSchema);
 const app = express();
 
 app.use(express.json());
+
+app.post("/api/users/lists/movies/add", async (req, res) => {
+  const bodyWithoutUserId = req.body;
+  const userId = req.body.userId;
+  delete bodyWithoutUserId.userId;
+
+  const existingMovie = await Movie.findOne({
+    TMDBId: req.body.TMDBId,
+  });
+
+  const newMovie = new Movie(bodyWithoutUserId);
+
+  if (!existingMovie) {
+    await newMovie.save();
+  }
+
+  const movieList = await MovieList.findOne({ userId: userId });
+
+  if (!movieList) {
+    const newMovieList = new MovieList({
+      userId: userId,
+      movies: [
+        {
+          id: existingMovie ? existingMovie._id : newMovie._id,
+          status: "a voir",
+          TMDBId: req.body.TMDBId,
+        },
+      ],
+    });
+
+    const user = await User.findById(userId);
+    user.moviesList = newMovieList._id;
+    await user.save();
+
+    await newMovieList.save();
+
+    return res.json({
+      message: "Film ajouté avec succès a la liste de l'utilisateur",
+      data: newMovieList,
+    });
+  }
+
+  const existingMovieInList = movieList.movies.find(
+    (movie) =>
+      movie.id.toString() ===
+      (existingMovie ? existingMovie._id : newMovie._id).toString()
+  );
+
+  if (existingMovieInList) {
+    return res.status(400).json({
+      message: "Ce film est déjà dans la liste de l'utilisateur",
+      error: true,
+    });
+  }
+
+  movieList.movies.push({
+    id: existingMovie ? existingMovie._id : newMovie._id,
+    status: "a voir",
+  });
+
+  await movieList.save();
+
+  res.json({
+    message: "Film ajouté avec succès a la liste de l'utilisateur",
+    data: movieList,
+  });
+});
+
+app.post("/api/users/lists/movies/update", async (req, res) => {
+  const userId = req.body.userId;
+  const TMDBId = Number(req.body.TMDBId);
+  const status = req.body.status;
+
+  const movieList = await MovieList.findOne({ userId: userId });
+
+  if (!movieList) {
+    return res.status(400).json({
+      message: "La liste de l'utilisateur n'existe pas",
+      error: true,
+    });
+  }
+
+  const movie = movieList.movies.find((movie) => {
+    console.log(movie.TMDBId, TMDBId);
+    return movie.TMDBId === TMDBId;
+  });
+
+  if (!movie) {
+    return res.status(400).json({
+      message: "Ce film n'est pas dans la liste de l'utilisateur",
+      error: true,
+    });
+  }
+
+  movie.status = status;
+
+  await movieList.save();
+
+  res.json({
+    message: "Statut du film mis à jour avec succès",
+    data: movieList,
+  });
+});
 
 app.post("/api/users", async (req, res) => {
   const user = new User(req.body);
