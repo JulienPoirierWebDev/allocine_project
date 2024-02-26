2c333895a8bf47ee4440753a3438079d1b583f1a
Visoth YOUKSIM <115548934+visothyouksim@users.noreply.github.com>
2024-02-26 15:04:34 +0100

    Update index.js

--- Cours_01/index.js
+++ Cours_01/index.js
@@ -1,10 +1,15 @@
+// Importer le module Express pour créer un serveur web
 const express = require("express");
+// Importer le module Mongoose pour interagir avec MongoDB
 const mongoose = require("mongoose");
 
+// Créer une instance d'application Express
 const app = express();
 
+// Fonction pour se connecter à la base de données MongoDB
 const dbConnexion = async () => {
   try {
+    // Se connecter à la base de données MongoDB
     await mongoose.connect(
       "mongodb+srv://visothyouksim:S0n4-g12@cluster0.bt5qjav.mongodb.net/crud-mongoose"
     );
@@ -15,8 +20,10 @@ const dbConnexion = async () => {
   }
 };
 
+// Appeler la fonction pour se connecter à la base de données
 dbConnexion();
 
+// Définir le schéma pour les utilisateurs dans MongoDB
 const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
@@ -24,18 +31,17 @@ const userSchema = mongoose.Schema({
   birthday: String,
 });
 
+// Créer un modèle Mongoose basé sur le schéma défini
 const Users = mongoose.model("Users", userSchema);
 
-// accepter les corps de requêtes (req.body)
+// Configurer Express pour accepter les corps de requêtes JSON
 app.use(express.json());
-
-// accepter les données issues des formulaires HTML
+// Configurer Express pour accepter les données de formulaire HTML
 app.use(express.urlencoded({ extended: false }));
 
-// #################### CREATE ###############################################################################################################################################
-
+// Route pour créer un nouvel utilisateur ##########################################################################################################################
 app.post("/api/users/create", async (request, response) => {
-  // fail fast
+  // Vérifier si les données requises sont présentes
   if (!request.body.email || !request.body.name || !request.body.password) {
     return response.json({
       message: "Il manque l'une des données",
@@ -43,29 +49,30 @@ app.post("/api/users/create", async (request, response) => {
     });
   }
 
+  // Vérifier si l'email est déjà utilisé
   const emailAlreadyUsed = await Users.find({ email: request.body.email });
 
   if (emailAlreadyUsed.length > 0) {
     return response.json({ message: "L'email est déjà utilisée", error: true });
   }
 
+  // Créer un nouvel utilisateur
   const newUser = new Users({
     name: request.body.name,
     email: request.body.email,
     password: request.body.password,
   });
 
-  console.log(emailAlreadyUsed);
-
+  // Sauvegarder le nouvel utilisateur dans la base de données
   await newUser.save();
 
+  // Répondre avec un message de succès
   response
     .status(201)
     .json({ message: "Utilisateur enregistré", error: false });
 });
 
-// #################### READ ALL ###############################################################################################################################################
-
+// Route pour récupérer tous les utilisateurs ####################################################################################################################
 app.get("/api/users", async (request, response) => {
   try {
     const users = await Users.find({});
@@ -78,8 +85,7 @@ app.get("/api/users", async (request, response) => {
   }
 });
 
-// ################################### FINDONE BY ID  #############################################################################################################################
-
+// Route pour trouver un utilisateur par ID ##########################################################################################################################
 app.get("/api/users/:id", async (request, response) => {
   try {
     const { id } = request.params;
@@ -100,8 +106,7 @@ app.get("/api/users/:id", async (request, response) => {
   }
 });
 
-// ############## UPDATE BY ID ##############################################################################################################################################
-
+// Route pour mettre à jour un utilisateur par ID ###################################################################################################################
 app.put("/api/users/update/:id", async (request, response) => {
   try {
     const { id } = request.params;
@@ -128,8 +133,7 @@ app.put("/api/users/update/:id", async (request, response) => {
   }
 });
 
-//   #################### DELETE BY ID #########################################################################################################################################
-
+// Route pour supprimer un utilisateur par ID #########################################################################################################################
 app.delete("/api/users/delete/:id", async (request, response) => {
   try {
     const { id } = request.params;
@@ -152,11 +156,10 @@ app.delete("/api/users/delete/:id", async (request, response) => {
   }
 });
 
-// ############################################################################################
-
+// Démarrer le serveur sur le port  3001
 app.listen("3001", (err) => {
   if (err) {
     console.log(err);
   }
-  console.log("Serveur lançé sur le port 3001");
+  console.log("Serveur lançé sur le port  3001");
 });
