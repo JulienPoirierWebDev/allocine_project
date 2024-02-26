edaf46c9e047a5946b683de57a8112acee136916
Kevin g <tsankera@net-c.com>
2024-02-26 16:11:29 +0100

    ajouts des dossiers models et controller

--- montest/index.js
+++ montest/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
+const User = require(../models/UserModel.js)
 
 const app = express();
 
 const dbConnexion = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://kevindabs:1234@cluster1.rylpson.mongodb.net/crud-mongoose"
+      "mongodb+srv://tsankera:1234@cluster01.9p5o38i.mongodb.net/crud-mongoose"
     );
 
     console.log("DB connexion succed");
@@ -34,11 +35,41 @@ app.use(express.urlencoded({ extended: false }));
 
 app.post("/api/users", async (request, response) => {
   // fail fast
+  if (request.body.email || !request.body.name || !request.body.password) {
+    return response.json({
+        message: "Il manque l'une des données",
+        error: true,
+    })
+  }
   const emailAlreadyUsed = await User.find({ email: request.body.email });
 
   if (emailAlreadyUsed.length > 0) {
     return response.json({ message: "L'email est déja utilisé", error: true });
   }
+app.get("/api/users", async (request, response) => {
+    const allUsers = await User.find();
+
+    if(allUsers.length === 0) {
+        response.json({message: "Pas d'utilisateurs", error: false});
+    }
+
+    response.json({ data: allUsers, error: false})
+})
+
+app.get("/api/users/:id", async (request, response) => {
+    try {
+        //Vérifier s'il y a bien un ID
+        const id = request.params.id;
+
+        //check if id is valid mongoose id
+        if (!mongoose.Types)
+    } catch (error) {
+        
+    }
+})
+
+
+
 
   const newUser = new User({
     name: request.body.name,
---
+++ montest/models/UserModel.js
@@ -0,0 +1,7 @@
+const userSchema = mongoose.Schema({
+    name: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    birtday: String,
+  });
+  
