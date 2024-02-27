e29941a21602ef495f241a178dbea5d9d560aede
Visoth YOUKSIM <115548934+visothyouksim@users.noreply.github.com>
2024-02-27 12:31:13 +0100

    start REACT

---
+++ REACT_01/src/App.jsx
@@ -0,0 +1,95 @@
+function App() {
+  return (
+    <>
+    <div style={{ backgroundColor: "black" }} className="container-fluid py-5">
+      <div className="row d-flex justify-content-center">
+        <div className="row d-flex justify-content-around">
+          <div
+            className="card col-md-6 col-sm-6 border-light mx-5 my-3"
+            style={{ width: "40rem", height: "50rem", backgroundColor: "black", color: "white" }}
+          >
+            <h2>La longue marche des chats</h2>
+            <img src="./public/chat1.jpg" alt="" />
+            <p>Les petits chats mignons envahissent internet</p>
+            <h3 className="fw-bold">
+              C'est un vrai phénomène de société, selon le Polithologue Alain
+              Chabat
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              ducimus soluta ea impedit iure culpa molestiae quae fuga ad ex!
+              Provident vero non aspernatur labore?
+            </p>
+          </div>
+
+          <div
+            className="card col-md-6 col-sm-6 border mx-5 my-3"
+            style={{ width: "40rem", height: "50rem", backgroundColor: "black", color: "white" }}
+          >
+            <h2>La longue marche des chats</h2>
+            <img src="./public/chat2.jpg" alt="" />
+            <p>Les petits chats mignons envahissent internet</p>
+            <h3 className="fw-bold">
+              C'est un vrai phénomène de société, selon le Polithologue Alain
+              Chabat
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              est odit quam obcaecati repudiandae quaerat eos sunt adipisci
+              recusandae eius sapiente maxime necessitatibus, consequuntur
+              voluptate provident ab accusantium repellendus quia quasi officiis
+              cumque non vitae unde laborum! Nisi praesentium perferendis
+              necessitatibus velit minima, doloribus sint, similique maiores
+              recusandae, explicabo ipsum.
+            </p>
+          </div>
+        </div>
+
+        {/* ####################################################################################################################################### */}
+
+        <div className="row d-flex justify-content-around">
+          <div
+            className="card col-md-6 col-sm-6 border mx-5 my-3"
+            style={{ width: "40rem", height: "50rem", backgroundColor: "black", color: "white" }}
+          >
+            <h2>La longue marche des chats</h2>
+            <img src="./public/chat3.jpg" alt="" />
+            <p>Les petits chats mignons envahissent internet</p>
+            <h3 className="fw-bold">
+              C'est un vrai phénomène de société, selon le Polithologue Alain
+              Chabat
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              est odit quam obcaecati repudiandae quaerat eos sunt adipisci
+              recusandae eius sapiente maxime necessitatibus, consequuntur
+              voluptate provident ab accusantium repellendus quia quasi officiis
+              cumque non vitae unde laborum! Nisi praesentium perferendis
+              necessitatibus velit minima, doloribus sint, similique maiores
+              recusandae, explicabo ipsum.
+            </p>
+          </div>
+
+          <div
+            className="card col-md-6 col-sm-6 border mx-5 my-3"
+            style={{ width: "40rem", height: "50rem", backgroundColor: "black", color: "white" }}
+          >
+            <h2>La longue marche des chats</h2>
+            <img src="./public/chat4.jpg" alt="" />
+            <p>Les petits chats mignons envahissent internet</p>
+            <h3 className="fw-bold">
+              C'est un vrai phénomène de société, selon le Polithologue Alain
+              Chabat
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore pariatur, et saepe illo, suscipit quisquam asperiores numquam, quos tempora architecto odit doloremque ea deleniti officia?
+            </p>
+          </div>
+        </div>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
