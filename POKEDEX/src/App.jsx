b59361709520f9b3b54fdb1cd72fdfce6913ba39
Visoth YOUKSIM <115548934+visothyouksim@users.noreply.github.com>
2024-03-04 09:36:45 +0100

    update_presentation

---
+++ POKEDEX/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PresentationPokedex from "../components/PresentationPokedex/PresentationPokedex";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="presentation">
+          <PresentationPokedex />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
